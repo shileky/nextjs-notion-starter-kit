f73b228486156febfbe7404dad7227c3d2b5ccbb
shileky <112742153+shileky@users.noreply.github.com>
2023-12-06 09:47:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,7 +17,7 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  Email: 'sivxiu@foxmail.com',
+  twitter: '',
   github: '',
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
